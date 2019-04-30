8915932d78caffbafbfa0c481ac623745a26565c
maxim_mariukhin <maryuhin.z@gmail.com>
2019-04-30 11:19:59 +0300

    Updates

---
+++ precache-manifest.d4985661d95fb37137bc372e572c2431.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d92f35f1efebc38103b8",
+    "url": "/goit-react-hw-02/static/js/runtime~main.577bbb3b.js"
+  },
+  {
+    "revision": "f6fbbe1e25a6f99f1b83",
+    "url": "/goit-react-hw-02/static/js/main.1940d247.chunk.js"
+  },
+  {
+    "revision": "c1129e4eaaf39820a755",
+    "url": "/goit-react-hw-02/static/js/2.1d62bf6b.chunk.js"
+  },
+  {
+    "revision": "f6fbbe1e25a6f99f1b83",
+    "url": "/goit-react-hw-02/static/css/main.12671775.chunk.css"
+  },
+  {
+    "revision": "980d3ebe50fa3ba703dcdc6e2f378513",
+    "url": "/goit-react-hw-02/index.html"
+  }
+];
